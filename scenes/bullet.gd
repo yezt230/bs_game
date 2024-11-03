2692163bde81397a47a16b74531641ad58f1dbbd
yezt230 <mjollnirsandwich@gmail.com>
2024-11-03 17:48:41 -0600

    Various progress on bullet detection

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -10,15 +10,11 @@ var direction = Vector2.RIGHT
 @onready var timer = $Timer
 
 func _process(_delta):
-	move_bullet()
-
-#func _physics_process(delta):
-	#var collision = move_and_collide(velocity * delta)
-	#if collision:
-		#print("collided with: ", collision.get_collider().name)
-
+	move_bullet(_delta)
+	
 
 func _ready():
+	#connect("body_entered", Callable(self, "_on_body_entered"))
 	_draw()
 	#timer.start()
 
@@ -32,16 +28,20 @@ func set_direction(updated_direction: int):
 	direction = updated_direction
 
 
-func move_bullet():
+func move_bullet(delta):
 	#velocity.x = MOVE_SPEED
 	velocity.x = direction * MOVE_SPEED
-	#print("thingy")
-	move_and_slide()
-	for i in get_slide_collision_count():
-		print("collided")
-		var collision = get_slide_collision(i)
-		print("Collided with: ", collision.get_collider().name)
-
+	#move_and_slide()
+	#for i in get_slide_collision_count():
+		#var collision = get_slide_collision(i)
+		#print("I collided with ", collision.get_collider().name)
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		print("collied")
 
 func _on_timer_timeout():
 	queue_free()
+
+
+#func _on_body_entered(body):
+	#print("Collision detected2with:", body.name)
--- scenes/stationary_enemy.gd
+++ scenes/stationary_enemy.gd
@@ -1,1 +1,23 @@
-extends CharacterBody2D
+extends Area2D
+
+var health := 3
+
+func _ready():
+	#connect("body_entered", Callable(self, "_on_body_entered"))
+	modulate = Color(1,1,1,1)
+
+func take_damage():
+	if health > 0:
+		health -= 1
+		update_tint()
+
+	if health <= 0:
+		die()
+
+func update_tint():
+	var red_tint := 1 - (health * 0.33)
+	modulate = Color(1, red_tint, red_tint, 1)
+
+
+func die():
+	queue_free()
