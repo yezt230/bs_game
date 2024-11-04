1a0653bbddaa848340b10c55aefedfd337e6a501
yezt230 <mjollnirsandwich@gmail.com>
2024-11-03 19:33:39 -0600

    Collision detection btwn bullet and enemy, adds red tint to enemy, enemy dies when health depleted

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -11,16 +11,10 @@ var direction = Vector2.RIGHT
 
 func _process(_delta):
 	move_bullet()
-
-#func _physics_process(delta):
-	#var collision = move_and_collide(velocity * delta)
-	#if collision:
-		#print("collided with: ", collision.get_collider().name)
-
+	
 
 func _ready():
 	_draw()
-	#timer.start()
 
 
 func _draw():
@@ -28,19 +22,20 @@ func _draw():
 
 
 func set_direction(updated_direction: int):
-	#direction = updated_direction.normalized()
 	direction = updated_direction
 
 
 func move_bullet():
-	#velocity.x = MOVE_SPEED
 	velocity.x = direction * MOVE_SPEED
-	#print("thingy")
 	move_and_slide()
 	for i in get_slide_collision_count():
-		#print("collided")
 		var collision = get_slide_collision(i)
-		print("Collided with: ", collision.get_collider().name)
+		print("damage")
+		
+		if collision.get_collider().is_in_group("enemies"):
+			print("damage dealt")
+			collision.get_collider().take_damage()
+			print("Collided with: ", collision.get_collider().name)
 		queue_free()
 
 
