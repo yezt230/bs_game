bf7dc3c261ebf47388f6a8106324bf1755cd808c
yezt230 <mjollnirsandwich@gmail.com>
2026-05-13 18:27:48 -0500

    AttackController for player

---
+++ attack_controller.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@onready var player = get_tree().get_nodes_in_group("player")[0]
+
+func _ready():
+	print(player)
--- scenes/punch.gd
+++ scenes/punch.gd
@@ -2,3 +2,5 @@ extends State
 
 func enter() -> void:
 	super()
+	#if parent.is_on_floor():
+	parent.player_animations.play("punch")
